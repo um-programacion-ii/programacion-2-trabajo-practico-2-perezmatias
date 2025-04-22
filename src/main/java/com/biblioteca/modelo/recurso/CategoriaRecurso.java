29693c6883095925fa37f520ed27462af9dc6b4e
perezmatias <maag.perez@alumno.um.edu.ar>
2025-04-22 15:37:25 -0300

    feat: Definir enum CategoriaRecurso (Etapa 2)

---
+++ src/main/java/com/biblioteca/modelo/recurso/CategoriaRecurso.java
@@ -0,0 +1,25 @@
+package com.biblioteca.modelo.recurso;
+
+public enum CategoriaRecurso {
+    NOVELA,
+    CIENCIA_FICCION,
+    FANTASIA,
+    TERROR,
+    POESIA,
+    ENSAYO,
+    HISTORIA,
+    BIOGRAFIA,
+    CIENCIA,
+    TECNOLOGIA,
+    PROGRAMACION,
+    INFANTIL_JUVENIL,
+    ARTE,
+    MUSICA,
+    COCINA,
+    VIAJES,
+    REVISTA_CIENTIFICA,
+    REVISTA_ACTUALIDAD,
+    AUDIO_MUSICA,
+    AUDIO_NARRATIVA,
+    OTROS;
+}
